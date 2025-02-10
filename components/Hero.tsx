393dd15349d280be157f8493c59b7f6cc030cc67
rafia9005 <rafia9005@gmail.com>
2025-02-10 20:33:55 +0700

    ui(change words & description): change words and decs

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -8,16 +8,15 @@ export default function Hero() {
                 <div className="flex justify-center relative my-20 z-10">
                     <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
                         <TextGenerateEffect
-                            words="Hi there 👋, welcome to StreetAI Your Smart Location Assistant."
+                            words="Empowering developers worldwide with innovative open-source solutions and professional website & app development services."
                             className="text-center text-[40px] md:text-5xl lg:text-6xl font-bold"
                         />
 
                         <div className="text-center mt-4 dark:text-gray-400">
                             <p>
-                                StreetAI is an intelligent assistant that provides detailed information about any location you select. Explore the world with ease and uncover insights like never before.
+                                TuxedoLabs provides developers with the tools and resources they need to create, collaborate, and innovate in the open-source ecosystem. Explore high-quality projects, from development tools, libraries, and frameworks to full-fledged applications.
                             </p>
                         </div>
-                        {/*  */}
                     </div>
                 </div>
             </GridBackground>
